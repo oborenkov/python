c4064b0c558b4774c52ac091da34ef14aaab20e3
oborenkov <[oborenkov@yahoo.com]>
2023-05-22 00:10:19 +0200

    start

---
+++ lectures/main.py
@@ -0,0 +1,160 @@
+# n= 3.7
+# print(n)
+# n= 'ddddddd'
+# print(n)
+# n1= "aaaaaaa"
+# print(n1)
+
+# a=5
+# print(type(a))
+# a="SSSSS"
+# print(type(a))
+# a=5.01
+# print(type(a))
+
+# a = 'fd\'df'
+# print(a)
+# a1='fd "gbgbgb"df'
+# print(a1)
+# #print(3)
+# """
+# print(3)
+# print(4)
+# print(8)
+# """
+
+
+# # # print(3)  (cmd) k (cmd)c 
+# # print(4)
+# print(8)       #(cmd) k (cmd)u  
+
+# a =5
+# b= 1.7
+# c = 'hello'
+# print(a,'-',b,'-',c)
+# print(f"{a}-{b}-{c}")
+# print("{}-{}-{}".format(a,b,c))
+
+
+print('введите первое число: ')
+a = int(input())
+b =  int (input('введите второе число: '))
+print(a,'+',b,'=',a+b)
+
+# c=2.3
+# print(c)
+# print(type(c))
+# c=int(c)
+# print(c)
+# print(type(c))
+# c=str(c)
+# print(c+'.3')
+# print(type(c))
+# c=bool(c)
+# print(c)
+# print(type(c))
+
+# + сложение
+# - вычетание
+# * умножение
+# / деление (по умолчанию в вещественных числах)
+# % остаток от деления
+# // целочисленное диление
+# ** возведение в степень
+
+
+# # a=5.45665
+# # b = 2.5466
+# # print(a*b)
+# # print(round(a*b, 3))
+
+# iter = 2 
+# print(iter)
+# iter+=3
+# print(iter)
+# #iter-=5
+# #print(iter)
+# iter*=5
+# print(iter)
+# iter/=3
+# print(iter)
+# iter//=4
+# print(iter)
+# iter%=4
+# print(iter)
+# iter**=3
+# print(iter)
+"""
+> больше,>= больше или равно,< меньше,<= меньше или равно,== равно (проверяет на равенство),!= не равно(проверяет не равны ли значения),not не(отрицание),and и(конъюкция),or или(дизъюкция)
+"""
+
+
+# username = input('Введите имя: ')
+# if username == 'Маша':
+#     print('Ура это же Маша')
+# elif username == 'Марина':
+#     print('Я так Вас ждал Марина')
+# elif username == 'Меша':
+#     print('Миша - топ')
+# else:
+#     print('Привет',username)
+
+"""
+i = 0
+while i < 5:
+#     if i == 3:
+#         break
+    i = i +1
+else:
+    print('Пжалуй')
+    print('хватит ) ')
+print(i)    
+"""
+
+# n = int(input())
+# flag = True
+# i = 2
+# while flag:
+#     if n % i == 0: # если остаток при делении числа n на i равен 0
+#         flag = False
+#         print(i)
+#     elif i > n //2: # делитель числа не может превышать введенное число деленное на 2
+#         print(n)
+#         flag = False
+#     i += 1
+
+
+# range(3) 0,1,2
+# range(2,5) 2,3,4
+# range(1,10.2) 1,3,5,7,9
+# range(100,0,-20) 100,80,60,40,20
+
+# a = 'qwerty'
+# for i in a:
+#     print(i)
+
+# line = ""
+# for i in range(5):
+#     line =""
+#     for j in range(5):
+#         line += "*"
+#     print(line)    
+
+
+# text = 'СъЕШь ещё этих МяГкИх французких булок'
+# # print(len(text))
+# # print('ещё'in text)
+# # print(text.lower())
+# # print(text.upper())
+# print(text.replace('ещё','ЕЩЁ'))
+# print(text[:])
+# print(text[:2])
+# print(text[20:])
+# print(text[6:-18])
+# print(text[0:len(text):6])
+# print(text[::6])
+# print(text[2:9] + text[-5] + text[:2])
+# print(text)
+
+
+
