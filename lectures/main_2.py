d5ffa11836930bf82a49ee2d66665255b223f4bb
oborenkov <[oborenkov@yahoo.com]>
2023-06-12 18:03:20 +0200

    homework_4

---
+++ lectures/main_2.py
@@ -0,0 +1,62 @@
+# list_1 =[]
+# list_1 = list()
+
+# list_1 = [1,2,45,67,34,90]
+
+# for i in list_1:
+#     print(i)
+# print(len(list_1))
+# print(list_1[4])
+# print(list_1[-1])  Индексация с конца
+# list_2 = [2,5]
+# print(list_2)
+# list_2.append(17)
+# print(list_2)
+# list_3 = []
+# for i in range(5):
+#     list_3.append(i)
+#     print(list_3)
+
+# удаление последнего элемента списка
+# list_4 = [1,34,21,67,35]
+# print(list_4.pop())
+# print(list_4)
+# # удаление конкретного элемента списка
+# list_5 = [1,34,21,67,35]
+# print(list_5.pop(3))
+# print(list_5)
+# # добавление элемента на нужную позицию
+# list_6 = [1,34,21,67,35]
+# print(list_6.insert(2, 33))
+# print(list_6)
+
+# list_7 = [1,2,3,4,5,6,7,8,9,10]
+# print(list_7[0])
+# print(list_7[-5])
+# print(list_7[:])
+# print(list_7[1:])
+# print(list_7[:3])
+# print(list_7[len(list_7)-2:])
+# print(list_7[0:len(list_7):6])
+# print(list_7[: :6])
+
+# кортеж  неизменяемые списки
+# t = (5, )
+# print(type(t))
+
+# v = [1,34,67]
+# print(v)
+# print(type(v))
+
+# v = tuple(v)
+# print(v)
+# print(type(v))
+# a,b,c = v
+# print(a,b,c)
+
+# # словари
+
+
+
+
+
