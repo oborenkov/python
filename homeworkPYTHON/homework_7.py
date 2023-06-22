634e3e7e38b93fb9b5a125afe7bbde24b7c9c590
oborenkov <[oborenkov@yahoo.com]>
2023-06-22 21:15:34 +0200

    homework

--- homeworkPYTHON/homework_7.py
+++ homeworkPYTHON/homework_7.py
@@ -50,6 +50,7 @@
 # 5  10  15  20  25  30
 # 6  12  18  24  30  36
 
+
 # def print_operation_table(operation, num_rows=6, num_columns=6):
 #     for i in range(1, num_rows + 1):
 #         answer = []
